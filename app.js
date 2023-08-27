e93b64c315883c9a37adbff68fca0a442e79611b
KamilMarcinowski <kamilmarcinowski2006@gmail.com>
2023-08-27 22:13:45 +0200

    commit init

---
+++ app.js
@@ -0,0 +1,19 @@
+const result = document.getElementById("result");
+const colorVisualisation = document.getElementById("color");
+
+const red = document.getElementById("red");
+const green = document.getElementById("green");
+const blue = document.getElementById("blue");
+
+function SetColor()
+{
+    let color = "rgb("+ red.value + "," + green.value + "," + blue.value + ")";
+
+    alert(color);
+    colorVisualisation.style.backgroundColor = color;
+    result.value = color;
+}
+
+red.addEventListener("input", SetColor);
+green.addEventListener("input", SetColor);
+blue.addEventListener("input", SetColor);
