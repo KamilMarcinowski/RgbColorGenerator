53ccc4a8497a3f9bdf523734aed2bef49d87fd11
KamilMarcinowski <kamilmarcinowski2006@gmail.com>
2023-08-28 12:00:38 +0200

    commit1

--- app.js
+++ app.js
@@ -5,14 +5,46 @@ const red = document.getElementById("red");
 const green = document.getElementById("green");
 const blue = document.getElementById("blue");
 
+const copyBtn = document.getElementById("copy");
+
 function SetColor()
 {
     let color = "rgb("+ red.value + "," + green.value + "," + blue.value + ")";
 
     colorVisualisation.style.backgroundColor = color;
     result.value = color;
+
+    SaveData();
 }
+
+function Copy()
+{
+    result.select();
+    document.execCommand("copy");
+
+    SaveData();
+}
+
+function SaveData()
+{
+    localStorage.setItem("red", red.value);
+    localStorage.setItem("green", green.value);
+    localStorage.setItem("blue", blue.value);
+}
+
+function LoadData()
+{
+    red.value = localStorage.getItem("red");
+    green.value = localStorage.getItem("green");
+    blue.value = localStorage.getItem("blue");
+
+    SetColor();
+}
+
+window.addEventListener("load", LoadData);
 
 red.addEventListener("input", SetColor);
 green.addEventListener("input", SetColor);
 blue.addEventListener("input", SetColor);
+
+copyBtn.addEventListener("click", Copy);
